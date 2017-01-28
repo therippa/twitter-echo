e23d4c1dceca320e1e5e0580b97379690326f74a
Jason Baker <therippa@gmail.com>
2017-01-27 17:29:54 -0800

    switching to postgres

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
-from tinydb import TinyDB, Query
 import urllib2
 import twitter
 import config
 import logging
+import os
+import psycopg2
+import urlparse
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
@@ -17,14 +19,26 @@ log.addHandler(h)
 
 @sched.scheduled_job('interval', minutes=15)  # increasing this may have twitter rate-limit you
 def check_tweets():
-    db = TinyDB('db.json')
-
     api = twitter.Api(
             consumer_key=config.consumer_key,
             consumer_secret=config.consumer_secret,
             access_token_key=config.access_token_key,
-            access_token_secret=config.access_token_secret,
+            access_token_secret=config.access_token_secret
     )
+
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    try:
+        conn = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port
+        )
+    except:
+        print "Cannot connect to db"
 
     url = "https://twitter.com/%s" % config.handle
 
@@ -33,7 +47,8 @@ def check_tweets():
 
     tweets = soup.find_all("div", class_="original-tweet")[:15]  # increasing this may have twitter rate-limit you
     tweets.reverse()
-    existing = Query()
+
+    cur = conn.cursor()
 
     for tweet in tweets:
         try:
@@ -48,16 +63,22 @@ def check_tweets():
         tweet_text = tweet.find("p", class_="tweet-text").get_text()[:140]  # parse 140 character if tweet is too long
         tweet_id = tweet.attrs["data-tweet-id"]
         print tweet_id
-        if db.search(existing.id == tweet_id):
+        cur.execute("SELECT * FROM tweets where tweet_id = '%s'" % tweet_id)
+        if cur.rowcount:
             print "already exists"
         else:
-            print "tweeting " + tweet_id
+            print "tweeting %s" % tweet_id
             try:
                 api.PostUpdate(tweet_text)
-                db.insert({'id': tweet_id})
+                cur.execute("INSERT INTO tweets (tweet_id) VALUES (%s)" % tweet_id)
             except Exception, e:
                 print tweet_text
                 print "tweet failed: " + str(e)
         print "------------------"
 
+    conn.commit()
+    cur.close()
+    conn.close()
+
 sched.start()
+#check_tweets()
